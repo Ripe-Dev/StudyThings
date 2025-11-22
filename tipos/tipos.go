7e1dd0ae9b668f947d3168f353846f987a6c1de6
Felipe Rodrigues da Silva <yfeliperodrigues190904@gmail.com>
2025-11-22 10:32:18 -0300

    Rettirado do comentario

--- tipos/tipos.go
+++ tipos/tipos.go
@@ -1,20 +1,20 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+)
 
-// func main() {
-// 	// bool (True/False)
-// 	// fmt.Printf("type: %T-value: %v\n", true, true)
+func main() {
+	// bool (True/False)
+	fmt.Printf("type: %T-value: %v\n", true, true)
 
-// 	// String - sequencia de bytes (tudo que tiver entre aspas)
+	// String - sequencia de bytes (tudo que tiver entre aspas)
 
-// 	// fmt.Printf("Type: %T-value: %v\n", "oi123", "oi123")
+	fmt.Printf("Type: %T-value: %v\n", "oi123", "oi123")
 
-// 	//Int - Numero inteio escrito sem aspas
-// 	// fmt.Printf("Type: %T-value: %v\n", 1, 1)
+	// Int - Numero inteio escrito sem aspas
+	fmt.Printf("Type: %T-value: %v\n", 1, 1)
 
-// 	//Float - um numero que possua casas decimais/virgula (float64/float32)
-// 	// fmt.Printf("(Type: %T-value: %v\n", 1.233, 1.233)
-// }
+	// Float - um numero que possua casas decimais/virgula (float64/float32)
+	fmt.Printf("(Type: %T-value: %v\n", 1.233, 1.233)
+}
